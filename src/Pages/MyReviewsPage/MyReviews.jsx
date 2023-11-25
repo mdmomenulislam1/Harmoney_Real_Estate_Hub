57b24a0490b45733b81af9117bc7d989adcfe3c4
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-11-25 06:02:36 +0600

    Home Page done

---
+++ src/Pages/MyReviewsPage/MyReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyReviews = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">My Reviews Page</h2>
+    </div>
+  );
+};
+
+export default MyReviews;
---
+++ src/Pages/PropertyBought/PropertyBought.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PropertyBought = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Property Bought Page</h2>
+    </div>
+  );
+};
+
+export default PropertyBought;
