51986d1acb7345227206e461c40077e551748a01
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-11-25 16:23:49 +0600

    All route setup

--- src/Pages/DashBoard/Dashboard.jsx
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { AuthContext } from '../../Firebase/AuthProvider';
 
 const Dashboard = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleSignOut = () => {
+    logOut()
+      .then()
+      .catch()
+  }
 
 
   return (
@@ -9,7 +17,7 @@ const Dashboard = () => {
       <h2 className="text-3xl">DashBoard</h2>
       <div className="flex">
         <div className="h-screen w-1/3 ">
-          <ul className="menu p-5 shadow-lg h-full bg-gray-700 mt-4">
+          <ul className="menu p-5 shadow-lg h-full mt-4">
             <li> <NavLink to="/dashboard/myProfile" className="p-5 mb-2   font-bold rounded-lg bg-red-900 text-white">My Profile</NavLink></li>
             <li> <NavLink to="/dashboard/wishlist" className="p-5 mb-2   font-bold rounded-lg bg-red-900 text-white">Wishlist</NavLink></li>
             <li> <NavLink to="/dashboard/propertyBought" className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">Property Bought</NavLink></li>
@@ -25,7 +33,11 @@ const Dashboard = () => {
             <li> <NavLink to="/dashboard/manageProperties" className="p-5 mb-2   font-bold rounded-lg bg-red-900 text-white">Manage Properties</NavLink></li>
             <li> <NavLink to="/dashboard/manageUsers" className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">Manage Users</NavLink></li>
             <li> <NavLink to="/dashboard/manageReviews" className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">Manage Reviews</NavLink></li>
-          
+
+            <div className="divider"></div> 
+            <li> <NavLink to="/" className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">Home </NavLink></li>
+            <li> <NavLink to="/allProperties" className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">All Properties</NavLink></li>
+            <button onClick={handleSignOut} className="p-5  mb-2  font-bold rounded-lg bg-red-900 text-white">Log Out</button>
 
           </ul>
         </div>
--- src/Pages/MyProfilePage/MyProfile.jsx
+++ src/Pages/MyProfilePage/MyProfile.jsx
@@ -1,25 +1,30 @@
 import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet';
 import { AuthContext } from '../../Firebase/AuthProvider';
+import useUsers from '../../Hooks/useUsers';
 
 const MyProfile = () => {
+  const [userInfo] = useUsers([]);
   const { user } = useContext(AuthContext);
-  
+  const userData = userInfo?.filter((item) => item?.email === user?.email);
+
+  console.log(userData);
+  console.log(userInfo);
+
   return (
-    <div className="text-center">
+    <div className="text-center mx-10">
       <Helmet>
         <title>{'Corner Cafe | My Profile'}</title>
       </Helmet>
       <h1 className="text-center p-5 text-4xl font-bold border-b-8 border-r-8 text-yellow-600 rounded-2xl border-yellow-600 mt-8 md:mt-12 lg:mt-16 ">My Profile Page</h1>
 
-      <div>
-        <img src={user?.photoURL} alt="" className="rounded-lg mx-auto my-3" />
-        <div>
-
+      <div className="flex gap-5 justify-center items-center">
+        <img src={userData?.photoURL} alt="" className="rounded-lg mx-auto my-3 w-40 h-40" />
+        <div className="text-left">
+          <p className="my-3 font-bold">Welcome</p>
+          <p className="my-3 font-bold text-yellow-600">Name: {userData?.displayName}</p>
+          <p className="my-3 font-bold">Email: {userData?.email}</p>
         </div>
-        <p className="my-3 font-bold">Welcome</p>
-        <p className="my-3 font-bold text-yellow-600">Name: {user?.displayName}</p>
-        <p className="my-3 font-bold">Email: {user?.email}</p>
 
       </div>
 
